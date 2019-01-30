a234fa3131ae196f730601a9b8664202da26e455
Michael Andrews <mike.andrews357@gmail.com>
2019-01-30 10:54:34 -0500

    add bootstrap and simple form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_30_024849) do
+ActiveRecord::Schema.define(version: 2019_01_30_152610) do
+
+  create_table "costs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "age"
+    t.string "babysitter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "hosts", force: :cascade do |t|
     t.string "title"
